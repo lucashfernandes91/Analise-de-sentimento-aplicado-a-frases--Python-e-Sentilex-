6089c3c00b201f0c59404ede65d156ae2c9edde9
Lucas Fernandes <lucashfernandes@yahoo.com.br>
2018-06-25 19:16:28 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+sentilexpt = open("SentiLex-lem-PT01.txt", 'r')
+dic_palavra_polaridade = {}
+# Neste trecho atribuímos a cada palavra uma Polaridade
+for i in sentilexpt.readlines():
+    pos_ponto = i.find('.')
+    palavra = (i[:pos_ponto])
+    pol_pos = i.find('POL')
+    polaridade = (i[pol_pos+4:pol_pos+6]).replace(';','')
+    dic_palavra_polaridade[palavra] = polaridade
+
+#print(dic_palavra_polaridade)
+#print(dic_palavra_polaridade.get('abandonado'))
+
+
+def Pontuação_sentimento(frase):
+    frase = frase.lower()
+    l_sentimento = []
+
+    for p in frase.split():
+        l_sentimento.append(int(dic_palavra_polaridade.get(p, 0)))
+
+    pontuacao = sum(l_sentimento)
+    if pontuacao > 0:
+        return 'Positivo, Score: {}'.format(pontuacao)
+    elif pontuacao == 0:
+        return 'Neutro, Score{}'.format(pontuacao)
+    elif pontuacao < 0:
+        return 'Negativo, Score{}'.format(pontuacao)
+
+
+Pontuação_sentimento('Eu estou muito feliz hoje, porém, triste com a politica')
+
+#dic_palavra_polaridade.get(‘feliz’)
+#dic_palavra_polaridade.get(‘triste’)
